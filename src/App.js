de337f299f2511255e2ffce45faa115cdf253546
Areeba Farhan1234 <areebakhan1454771@gmail.com>
2024-06-13 14:02:49 +0500

    CHANGE

--- src/App.js
+++ src/App.js
@@ -1,25 +1,103 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'; // Make sure to add your CSS for the classes
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  // const words = ["React Developer", "React  Developer", " & UX &nbsp; Designer"]; // Replace with your actual words
+  // const [currentWordIndex, setCurrentWordIndex] = useState(0);
+
+  // useEffect(() => {
+  //   // Split words into letters and wrap them in spans on component mount
+  //   const wordElements = document.querySelectorAll('.word');
+  //   wordElements.forEach((word) => {
+  //     let letters = word.textContent.split("");
+  //     word.textContent = "";
+  //     letters.forEach((letter) => {
+  //       let span = document.createElement("span");
+  //       span.textContent = letter;
+  //       span.className = letter;
+  //       word.appendChild(span);
+  //     });
+  //   });
+
+  //   // Set the opacity of the first word
+  //   if (wordElements[0]) {
+  //     wordElements[0].style.opacity = "1";
+  //   }
+
+  //   // Set up interval for text change
+  //   const interval = setInterval(() => {
+  //     changeText();
+  //   }, 4000); // Change text every 4 seconds
+
+  //   return () => clearInterval(interval); // Cleanup interval on component unmount
+  // }, [currentWordIndex]);
+
+  // const changeText = () => {
+  //   const wordElements = document.querySelectorAll('.word');
+  //   const maxWordIndex = wordElements.length - 1;
+
+  //   const currentWord = wordElements[currentWordIndex];
+  //   const nextWord = currentWordIndex === maxWordIndex ? wordElements[0] : wordElements[currentWordIndex + 1];
+
+  //   Array.from(currentWord.children).forEach((letter, i) => {
+  //     setTimeout(() => {
+  //       letter.className = "Letter out";
+  //     }, i * 80);
+  //   });
+
+  //   nextWord.style.opacity = "1";
+  //   Array.from(nextWord.children).forEach((letter, i) => {
+  //     letter.className = "Letter behind";
+  //     setTimeout(() => {
+  //       letter.className = "Letter in";
+  //     }, 340 + i * 80);
+  //   });
+
+  //   setCurrentWordIndex(currentWordIndex === maxWordIndex ? 0 : currentWordIndex + 1);
+  // };
+
+  // return (
+  //   <div className="word-container">
+  //     {words.map((word, index) => (
+  //       <div className="word" key={index} style={{ opacity: index === 0 ? 1 : 0 }}>
+  //         {word}
+  //       </div>
+  //     ))}
+  //   </div>
+  // );
+
+//   const word = document.querySelectorAll(".word");
+//   const changeWord = () => {
+//     setTimeout(() => {
+//       word.wordContent = "React&nbsp;Developer";
+//     }, 0);
+//     setTimeout(() => {
+//       word.wordContent = "Frontend &nbsp; React &nbsp; Developer ";
+//     }, 400);
+//     setTimeout(() => {
+//       word.wordContent = " UI & UX &nbsp; Designer ";
+//     },800);
+//     setTimeout(() => {
+//       word.wordContent = "  Web &nbsp; Designer ";
+//     }, 1200);
+//   }
+//   changeWord();
+
+const circles = document.querySelectorAll('circle');
+circles.forEach(elem =>{
+  var dots = elem.getAttribute("data-dots");
+  var marked = elem.getAttribute("data-percent");
+  var percent = Math.floor(dots*marked/100);
+  var points = "";
+  var rotate = 360/dots;
+
+
+  for(let i = 0; i < dots; i++) {
+    points += `<div className="points" style={style='--i:${i}; --rot:${rotate}deg'}></div>`;
+  }
+  elem.innerHTML = points;
+})
+};
+
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,269 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <>
+    
+          {/* NAVBAR-SECTION */}
+        <header>
+            <div className='logo'>
+              <span>A</span>reeba.
+            </div>
+            <ul className='navList'>
+              <li><a href="#home">Home</a></li>
+              <li><a href="#about">About</a></li>
+              <li><a href="#services">Services</a></li>
+              <li><a href="#skills">Skills</a></li>
+              <li><a href="#portfolio">Portfolio</a></li>
+              <li><a href="#contact">Contact</a></li>
+            </ul>
+
+
+            <div id="menu-icon" className='bx bx-menu'>
+            </div>
+          </header>
+
+            {/* HOME-SECTION */}
+
+          <section id="home" className="home">
+            <div className="home-content">
+              <h1>Hi I'm Areeba Farhan</h1>
+              <div className="change-text">
+                <h3> And I'm &nbsp;</h3>
+                <h3>
+                  <span className="word">
+                 
+                  </span>
+                </h3>
+              </div>
+
+              <p>Product Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis ea itaque est? Suscipit placeat architecto est vitae, aperiam tempora delectus quasi, earum, numquam nulla nemo quod? Sed officia in tempore.</p>
+
+
+              <div className="btn-box">
+                <a href="/btn" className="btn">Download CV</a>
+                <a href="/btn" className="btn">Hire Me Now!</a>
+              </div>
+
+
+              <div className="social-icons">
+                <a href="/facebook" title='facebook'><i class='bx bxl-facebook'></i></a>
+                <a href="/instagram"  title='instagram'><i class='bx bxl-instagram' ></i></a>
+                <a href="/linkedin"  title='linkedin'><i class='bx bxl-linkedin' ></i></a>
+                <a href="/github"  title='github'><i class='bx bxl-github' ></i></a>
+              </div>
+            </div>
+
+            <div className="home-image">
+              <div className="img-box">
+                <img src='img/1.png' alt='home'/>
+              </div>
+            </div>
+          </section>
+
+            {/* ABOUT-SECTION */}
+
+          <section id="about" className="about">
+            <div className="img-box">
+
+              <img src="/img/12.png" alt="" />
+
+            </div>
+
+            <div className="about-content">
+              <span>Let me introduce myself</span>
+              <h2>About Me</h2>
+              <h3>A story of good</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae maiores adipisci tenetur expedita doloribus, eos dicta officiis quisquam inventore excepturi quidem neque fugit aut voluptates aperiam sed molesti Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas suscipit fugiat tempore, eos delectus facilis commodi porro corrupti dolores, non ratione consectetur dignissimos odit! Assumenda reiciendis blanditiis dolor distinctio alias.ae esse reprehenderit.</p>
+              
+            <div className="btn-box">
+              <a href="/" className='btn'>Read More!</a>
+            </div>
+            </div>
+
+          </section>
+
+            {/* SERVICES-SECTION */}
+
+          <section id="services" className='services'>
+            <div className="main-text">
+              <span>What i will do for you</span>
+              <h2>Our Services</h2>
+            </div>
+
+            <div className="section_services">
+              <div className="service-box">
+                <i class='bx bxs-layer service-icon'></i>
+                <h3>React Developer</h3>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente maiores aliquam, excepturi ipsam perferendis totam. Vitae quaerat nostrum enim. Omnis dicta, dolores iusto similique possimus nisi excepturi itaque. Tempore, perferendis! Lorem ipsum dolor.
+                </p>
+                <div className="btn-box service-btn">
+                  <a href="/" className='btn'>Read More!</a>
+                </div>
+
+              </div>
+
+              <div className="service-box">
+                <i class='bx bx-desktop service-icon'></i>
+                <h3>React Developer</h3>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente maiores aliquam, excepturi ipsam perferendis totam. Vitae quaerat nostrum enim. Omnis dicta, dolores iusto similique possimus nisi excepturi itaque. Tempore, perferendis! Lorem ipsum dolor.
+                </p>
+                <div className="btn-box service-btn">
+                  <a href="/" className='btn'>Read More!</a>
+                </div>
+              
+              </div>
+
+              <div className="service-box">
+                <i class='bx bx-code-alt service-icon'></i>
+                <h3>React Developer</h3>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente maiores aliquam, excepturi ipsam perferendis totam. Vitae quaerat nostrum enim. Omnis dicta, dolores iusto similique possimus nisi excepturi itaque. Tempore, perferendis! Lorem ipsum dolor.
+                </p>
+                <div className="btn-box service-btn">
+                  <a href="/" className='btn'>Read More!</a>
+                </div>
+              
+              </div>
+
+            </div>
+
+          </section>
+
+          {/* SKILLS-SECTION */}
+
+          <section id="skills" className='skills'>
+            <div className="main-text">
+              <span>Technical and Professional</span>
+              <h2>My Skills</h2>
+            </div>
+
+            <div className="skill-main">
+              <div className="skill-left">
+                <h3>Technical Skills</h3>
+
+                <div className="skill-bar">
+                  <div className="info">
+                    <p>Html</p>
+                    <p>85%</p>
+                  </div>
+                  <div className="bar">
+                    <span className='html'></span>
+                  </div>
+                </div>
+
+                <div className="skill-bar">
+                  <div className="info">
+                    <p>CSS</p>
+                    <p>62%</p>
+                  </div>
+                  <div className="bar">
+                    <span className='css'></span>
+                  </div>
+                </div>
+
+                
+                <div className="skill-bar">
+                  <div className="info">
+                    <p>Bootstrap</p>
+                    <p>77%</p>
+                  </div>
+                  <div className="bar">
+                    <span className='bootstrap'></span>
+                  </div>
+                </div>
+
+                <div className="skill-bar">
+                  <div className="info">
+                    <p>Figma</p>
+                    <p>79%</p>
+                  </div>
+                  <div className="bar">
+                    <span className='figma'></span>
+                  </div>
+                </div>
+
+
+                <div className="skill-bar">
+                  <div className="info">
+                    <p>JavaScript</p>
+                    <p>57%</p>
+                  </div>
+                  <div className="bar">
+                    <span className='javascript'></span>
+                  </div>
+                </div>
+
+                
+                <div className="skill-bar">
+                  <div className="info">
+                    <p>React JS</p>
+                    <p>52%</p>
+                  </div>
+                  <div className="bar">
+
+                    <span className='react-js'></span>
+                  </div>
+                </div>
+
+              </div>
+
+              <div className="skill-right">
+              <h3>Professional Skills</h3>
+              <div className="professional">
+                <div className="box">
+                <div className="circle"  data-dots={"80"} data-percen={"90"}></div>
+
+                  <div className="text">
+                    <big>90%</big>
+                    <small>Team Work</small>
+                  </div>
+                </div>
+
+                <div className="box">
+                <div className="circle"  data-dots={"80"} data-percen={"90"}></div>
+
+                  <div className="text">
+                    <big>90%</big>
+                    <small>Team Work</small>
+                  </div>
+                </div>
+
+                <div className="box">
+                <div className="circle"  data-dots={"80"} data-percen={"90"}></div>
+
+                  <div className="text">
+                    <big>90%</big>
+                    <small>Team Work</small>
+                  </div>
+                </div>
+
+                <div className="box">
+                  <div className="circle" data-dots={"80"} data-percen={"90"}></div>
+
+                  <div className="text">
+                    <big>90%</big>
+                    <small>Team Work</small>
+                  </div>
+                </div>
+
+                
+              </div>
+              </div>
+            </div>
+
+          </section>
+
+    </>
+
   </React.StrictMode>
 );
 
